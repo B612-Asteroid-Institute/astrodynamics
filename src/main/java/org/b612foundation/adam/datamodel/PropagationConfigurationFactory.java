0909351295275f90848e9133970a26d70d24f4b2
Hank Grabowski <hankgrabowski@gmail.com>
2019-10-16 20:18:59 -0400

    Add PropagatorConfigurationFactory and test

---
+++ src/main/java/org/b612foundation/adam/datamodel/PropagationConfigurationFactory.java
@@ -0,0 +1,43 @@
+package org.b612foundation.adam.datamodel;
+
+/**
+ * Factory class which produces common configurations for the propagators.
+ */
+public class PropagationConfigurationFactory {
+
+  private static final PropagatorConfiguration ALL_MAJOR_BODIES_CONFIG =
+          new PropagatorConfiguration()
+                  .setSun(PropagatorConfiguration.PlanetGravityMode.POINT_MASS)
+                  .setMercury(PropagatorConfiguration.PlanetGravityMode.POINT_MASS)
+                  .setVenus(PropagatorConfiguration.PlanetGravityMode.POINT_MASS)
+                  .setEarth(PropagatorConfiguration.PlanetGravityMode.POINT_MASS)
+                  .setMars(PropagatorConfiguration.PlanetGravityMode.POINT_MASS)
+                  .setJupiter(PropagatorConfiguration.PlanetGravityMode.POINT_MASS)
+                  .setSaturn(PropagatorConfiguration.PlanetGravityMode.POINT_MASS)
+                  .setUranus(PropagatorConfiguration.PlanetGravityMode.POINT_MASS)
+                  .setNeptune(PropagatorConfiguration.PlanetGravityMode.POINT_MASS)
+                  .setPluto(PropagatorConfiguration.PlanetGravityMode.POINT_MASS)
+                  .setMoon(PropagatorConfiguration.PlanetGravityMode.POINT_MASS);
+
+  private static final PropagatorConfiguration SUN_ONLY_CONFIG =
+          new PropagatorConfiguration()
+                  .setSun(PropagatorConfiguration.PlanetGravityMode.POINT_MASS)
+                  .setMercury(PropagatorConfiguration.PlanetGravityMode.OMIT)
+                  .setVenus(PropagatorConfiguration.PlanetGravityMode.OMIT)
+                  .setEarth(PropagatorConfiguration.PlanetGravityMode.OMIT)
+                  .setMars(PropagatorConfiguration.PlanetGravityMode.OMIT)
+                  .setJupiter(PropagatorConfiguration.PlanetGravityMode.OMIT)
+                  .setSaturn(PropagatorConfiguration.PlanetGravityMode.OMIT)
+                  .setUranus(PropagatorConfiguration.PlanetGravityMode.OMIT)
+                  .setNeptune(PropagatorConfiguration.PlanetGravityMode.OMIT)
+                  .setPluto(PropagatorConfiguration.PlanetGravityMode.OMIT)
+                  .setMoon(PropagatorConfiguration.PlanetGravityMode.OMIT);
+
+  public static PropagatorConfiguration getAllMajorBodiesConfig() {
+    return ALL_MAJOR_BODIES_CONFIG;
+  }
+
+  public static PropagatorConfiguration getSunOnlyConfig() {
+    return SUN_ONLY_CONFIG;
+  }
+}
--- src/test/java/org/b612foundation/adam/datamodel/estimation/OorbConfigWriterTest.java
+++ src/test/java/org/b612foundation/adam/datamodel/estimation/OorbConfigWriterTest.java
@@ -1,5 +1,6 @@
 package org.b612foundation.adam.datamodel.estimation;
 
+import org.b612foundation.adam.datamodel.PropagationConfigurationFactory;
 import org.b612foundation.adam.datamodel.PropagatorConfiguration;
 import org.junit.Test;
 
@@ -7,39 +8,12 @@ import java.io.IOException;
 import java.nio.file.Files;
 
 public class OorbConfigWriterTest {
-    private PropagatorConfiguration allMajorBodiesConfig =
-            new PropagatorConfiguration()
-                    .setSun(PropagatorConfiguration.PlanetGravityMode.POINT_MASS)
-                    .setMercury(PropagatorConfiguration.PlanetGravityMode.POINT_MASS)
-                    .setVenus(PropagatorConfiguration.PlanetGravityMode.POINT_MASS)
-                    .setEarth(PropagatorConfiguration.PlanetGravityMode.POINT_MASS)
-                    .setMars(PropagatorConfiguration.PlanetGravityMode.POINT_MASS)
-                    .setJupiter(PropagatorConfiguration.PlanetGravityMode.POINT_MASS)
-                    .setSaturn(PropagatorConfiguration.PlanetGravityMode.POINT_MASS)
-                    .setUranus(PropagatorConfiguration.PlanetGravityMode.POINT_MASS)
-                    .setNeptune(PropagatorConfiguration.PlanetGravityMode.POINT_MASS)
-                    .setPluto(PropagatorConfiguration.PlanetGravityMode.POINT_MASS)
-                    .setMoon(PropagatorConfiguration.PlanetGravityMode.POINT_MASS);
-
-    private PropagatorConfiguration justTheSunConfig =
-            new PropagatorConfiguration()
-                    .setSun(PropagatorConfiguration.PlanetGravityMode.POINT_MASS)
-                    .setMercury(PropagatorConfiguration.PlanetGravityMode.OMIT)
-                    .setVenus(PropagatorConfiguration.PlanetGravityMode.OMIT)
-                    .setEarth(PropagatorConfiguration.PlanetGravityMode.OMIT)
-                    .setMars(PropagatorConfiguration.PlanetGravityMode.OMIT)
-                    .setJupiter(PropagatorConfiguration.PlanetGravityMode.OMIT)
-                    .setSaturn(PropagatorConfiguration.PlanetGravityMode.OMIT)
-                    .setUranus(PropagatorConfiguration.PlanetGravityMode.OMIT)
-                    .setNeptune(PropagatorConfiguration.PlanetGravityMode.OMIT)
-                    .setPluto(PropagatorConfiguration.PlanetGravityMode.OMIT)
-                    .setMoon(PropagatorConfiguration.PlanetGravityMode.OMIT);
 
     @Test
     public void testDefaultConfigFile() throws IOException {
         var config = OorbEstimateConfigurationFactory.buildDefaultConfig();
         var tmpConfig = Files.createTempFile("oorb", ".conf");
-        OorbConfigWriter.writeConfiguration(tmpConfig, config, allMajorBodiesConfig);
+        OorbConfigWriter.writeConfiguration(tmpConfig, config, PropagationConfigurationFactory.getAllMajorBodiesConfig());
         var lines = Files.readAllLines(tmpConfig);
         lines.forEach(System.out::println);
     }
