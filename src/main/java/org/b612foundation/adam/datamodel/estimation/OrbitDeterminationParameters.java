3f6b4247bf3e043eb84ba09acd08f17f505774a7
Hank Grabowski <hankgrabowski@gmail.com>
2019-12-12 07:06:52 -0500

    Add DES File Path to measurement types

--- src/main/java/org/b612foundation/adam/datamodel/estimation/OrbitDeterminationParameters.java
+++ src/main/java/org/b612foundation/adam/datamodel/estimation/OrbitDeterminationParameters.java
@@ -69,6 +69,7 @@ public class OrbitDeterminationParameters extends AdamObject {
     }
 
     public enum MeasurumentType {
-        LsstCsvFilePath
+        LsstCsvFilePath,
+        DesFilePath
     }
 }
